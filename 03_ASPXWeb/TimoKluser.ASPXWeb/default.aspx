97b4abba88b933c361bee5af884fcf5e845d1571
Timo Kluser <timo@timokluser.ch>
2021-08-17 15:35:37 +0200

    added 03_ASPXWeb

---
+++ 03_ASPXWeb/TimoKluser.ASPXWeb/default.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="TimoKluser.ASPXWeb._default" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title></title>
+</head>
+
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <p>
+                <input type="text" id="zahl1" runat="server" />
+            </p>
+            <p>
+                <input type="submit" value="Senden" runat="server" />
+            </p>
+
+            <p id="ergebnis" runat="server"></p>
+        </div>
+    </form>
+</body>
+</html>
---
+++ 03_ASPXWeb/TimoKluser.ASPXWeb/default.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TimoKluser.ASPXWeb
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack)
+            {
+                // bei Submit ausgeführt
+                try
+                {
+                    double z = double.Parse(zahl1.Value);
+                    ergebnis.InnerHtml = $"Die Eingabe ist: {z}";
+                }
+                catch (Exception)
+                {
+                    ergebnis.InnerHtml = $"No valid input";
+                }
+            }
+            else
+            {
+                // bei Seitenanforderung ausgeführt
+                ergebnis.InnerHtml = "Hier kommt das Ergebnis";
+            }
+
+        }
+    }
+}
