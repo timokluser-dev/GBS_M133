068deb1f689eaf7cff586a52bc225a98c86e5270
Timo Kluser <timo@timokluser.ch>
2021-08-24 18:09:23 +0200

    added 04_ASPXWebToday

---
+++ 04_ASPXWebToday/TimoKluser.ASPXWebToday/default.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TimoKluser.ASPXWebToday
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var formatedDate = DateTime.Now.ToString("dddd dd.MM.yyyy HH:mm:ss", new CultureInfo("en-US"));
+            lblToday.InnerText = $"Today is the {formatedDate}";
+        }
+    }
+}
