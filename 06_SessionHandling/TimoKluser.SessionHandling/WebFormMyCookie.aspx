3df21b4a807c5e46a7392e2a54f42ca4e70de83e
Timo Kluser <timo@timokluser.ch>
2021-08-31 15:19:58 +0200

    added 06_SessionHandling

---
+++ 06_SessionHandling/TimoKluser.SessionHandling/WebFormMyCookie.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebFormMyCookie.aspx.cs" Inherits="TimoKluser.SessionHandling.WebFormMyCookie" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="txtCookieValue" runat="server" />
+        </div>
+        <div>
+            <asp:Button Text="Create Cookie" ID="btnCreateCookie" runat="server" OnClick="btnCreateCookie_Click" />
+        </div>
+        <div>
+            <asp:Label Text="Cookie Value: " runat="server" />
+            <asp:Label Text="" ID="lblStatus" runat="server" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ 06_SessionHandling/TimoKluser.SessionHandling/WebFormMyCookie.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TimoKluser.SessionHandling
+{
+    public partial class WebFormMyCookie : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SetText();
+        }
+
+        protected void btnCreateCookie_Click(object sender, EventArgs e)
+        {
+            Response.Cookies["MyFirstCookie"].Value = txtCookieValue.Text;
+            Response.Cookies["MyFirstCookie"].Expires = DateTime.Now.AddMinutes(1);
+            Response.Cookies["MyFirstCookie"].Domain = "localhost";
+
+            SetText();
+        }
+
+        protected void SetText()
+        {
+            if (Request.Cookies["MyFirstCookie"] != null)
+            {
+                lblStatus.Text = Request.Cookies["MyFirstCookie"].Value;
+            }
+        }
+    }
+}
