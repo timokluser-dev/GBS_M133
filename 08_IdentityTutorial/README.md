fab89b1c4b5d65cb1c89bd6164536ebd2eabfa4f
Timo Kluser <timo@timokluser.ch>
2021-09-08 20:10:45 +0200

    added 08_IdentityTutorial

---
+++ 08_IdentityTutorial/README.md
@@ -0,0 +1,3 @@
+# Link to Tutorial
+
+:arrow_right: [https://docs.microsoft.com/en-us/aspnet/identity/overview/features-api/account-confirmation-and-password-recovery-with-aspnet-identity](https://docs.microsoft.com/en-us/aspnet/identity/overview/features-api/account-confirmation-and-password-recovery-with-aspnet-identity)
---
+++ 08_IdentityTutorial/TimoKluser.IdentityTutorial/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TimoKluser.IdentityTutorial.Startup))]
+namespace TimoKluser.IdentityTutorial
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
